12ffaa550208205df1c9d6247b0a2f36d1263ff4
Captain Burah <cptburah@gmail.com>
2022-01-05 22:04:01 +0530

    Footer modified

--- resources/views/front-end/index.blade.php
+++ resources/views/front-end/index.blade.php
@@ -611,9 +611,14 @@
     </div>
     <div class="row">
     <div class="col-md-12 text-center">
-
+        <p style="font-family: 'Caveat', cursive; font-weight: 400;">
+            a work of 
+            <a href="https://www.upwritsolutions.com" class="text-center text-decoration-none text-light" target="__blank">
+                UpWrit Solutions 
+            </a>
+        </p>
         <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        A product of <a href="https://www.upwritsolutions.com/">UpWrit Solutions</a> | All rights reserved &copy;<script>document.write(new Date().getFullYear());</script> | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+        All rights reserved &copy;<script>document.write(new Date().getFullYear());</script> | <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
     </div>
     </div>
